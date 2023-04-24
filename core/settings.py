fb105c286fc9d14868b0d83bcbedbe3b83b8cfdf
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-04-24 19:52:25 -0300

    feat: change db to sqlite

--- core/settings.py
+++ core/settings.py
@@ -107,7 +107,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///' + os.path.join('db.sqlite3'))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 STATICFILES_DIRS = os.path.join(BASE_DIR),
