61f826699f251a44ebb1c5f206b9acf9ac18bacd
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-04-24 14:46:54 -0300

    config static_root

--- core/settings.py
+++ core/settings.py
@@ -141,11 +141,12 @@ DATABASES = {
 #     db_from_env = dj_database_url.config(
 #         default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 #     DATABASES['default'].update(db_from_env)
-#     DEBUG = False
+    
+DEBUG = False
 
-# if not DEBUG:
-#     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-#     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
